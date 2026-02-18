28f7c3bacc1f87d2c2d262645292500a5bae593c
Shamim <samimtsbd19@gmail.com>
2026-02-18 07:29:40 -0800

    fix: remove duplicate Alpine.js

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -39,9 +39,6 @@
         }
     </script>
 
-    <!-- Alpine.js -->
-    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-
     @livewireStyles
 
     <style>
